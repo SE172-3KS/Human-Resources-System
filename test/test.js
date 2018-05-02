aacb32276786c59c08fb227d6514c83856097e97
haeol <k.tom_1@yahoo.com>
2018-05-02 16:50:14 -0700

    added db tests

--- test/test.js
+++ test/test.js
@@ -1,19 +1,31 @@
 import HttpService from '../src/services/http.service';
-
 let httpService = new HttpService();
+let mysql = require('mysql');
+
 
 var assert = require('assert');
+
+
+
 describe('Database Tests', function() {
-    var server;
-    beforeEach(function() {
-        server = require('../server');
-    });
-    afterEach(function() {
-        server.close();
-    });
+	var connection = mysql.createConnection({
+		host     : 'localhost',
+		user     : '3ks',
+		password : '123456',
+		database : 'employees'
+	});
+	connection.connect();
 
-    it('test1', function test1(done) {
-        request(server).get('/listEmployees').expect(200, done);
-    });
-
+	describe('Test schemas', function() {
+		it('test1', function() {
+	        connection.query('select * from employees limit 30;', (err, rows, fields)=> {
+				assert.equal(err, null);
+	        });
+		});
+        it('test2', function() {
+            connection.query("Select * from payouts", (err, rows, fields) => {
+                response.json({payouts: rows});
+            });
+        });
+	});
 });
