e237dfee3ab3a72c0ef69b3110a06dc82e8fab57
Seling Chen <seling.chen@yahoo.com>
2018-05-03 00:39:06 -0700

    added api tests

--- test/test.js
+++ test/test.js
@@ -1,31 +1,35 @@
-import HttpService from '../src/services/http.service';
-let httpService = new HttpService();
+let axios = require('axios');
+let http = axios.create();
 let mysql = require('mysql');
-
 
 var assert = require('assert');
 
+describe('Database Tests', function () {
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: '3ks',
+    password: '123456',
+    database: 'employees'
+  });
+  connection.connect();
 
-
-describe('Database Tests', function() {
-	var connection = mysql.createConnection({
-		host     : 'localhost',
-		user     : '3ks',
-		password : '123456',
-		database : 'employees'
-	});
-	connection.connect();
-
-	describe('Test schemas', function() {
-		it('test1', function() {
-	        connection.query('select * from employees limit 30;', (err, rows, fields)=> {
-				assert.equal(err, null);
-	        });
-		});
-        it('test2', function() {
-            connection.query("Select * from payouts", (err, rows, fields) => {
-                response.json({payouts: rows});
-            });
-        });
-	});
+  describe('Test schemas', function () {
+    it('retrieve employees from database', function () {
+      connection.query('select * from employees limit 30;', (err, rows, fields) => {
+        assert.equal(err, null);
+      });
+    });
+    it('retrieve payout from database', function () {
+      connection.query("Select * from payouts", (err, rows, fields) => {
+        assert.equal(err, null);
+      });
+    });
+    it('retrieve payouts from server', function (done) {
+      http.get('http://localhost:3000/api/listPayouts').then((data) => {
+        done()
+      }).catch(err => {
+        done(err)
+      })
+    })
+  });
 });
